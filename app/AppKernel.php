a32da363206907a68a3baa9fdc48338b4ec1a632
Fred Cox <mcfedr@gmail.com>
2013-12-12 22:27:14 +0200

    Creating the HromPush bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new ekreative\AWSPushBundle\ekreativeAWSPushBundle(),
+            new mcfedr\AWSPushBundle\mcfedrAWSPushBundle(),
             new mcfedr\HromPushBundle\mcfedrHromPushBundle(),
         );
 
