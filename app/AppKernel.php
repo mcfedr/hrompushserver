911ecbf4ef36da603e09686a4bcdb9992e66ac4e
Fred Cox <mcfedr@gmail.com>
2014-03-06 21:38:58 +0200

    Enable the youtube streams bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new mcfedr\AWSPushBundle\mcfedrAWSPushBundle(),
             new mcfedr\TwitterPushBundle\mcfedrTwitterPushBundle(),
+            new mcfedr\YouTube\LiveStreamsBundle\mcfedrYouTubeLiveStreamsBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
